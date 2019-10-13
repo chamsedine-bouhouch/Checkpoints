a88c1796520fb545fcf463908f6d04be473fe228
chamseddine bouhouch <bouhouchchamseddine@gmail.com>
2019-10-13 21:41:32 +0200

    version final

--- ES6.js
+++ ES6.js
@@ -4,34 +4,13 @@ const pets = [
   { name: `Jasper`, type:`dog`, bornOn: 2016 }
 ];
 
-const getAge = (pet) => {
-  return new Date().getFullYear() - pet.bornOn;
-}
+const getAge = pet => {return new Date().getFullYear() - pet.bornOn;}
 
-let petsWithAge = [];
-pets.forEach((val, index ) => {
-  let pet = pets[index];
-  pet.age = getAge(pet);
-  petsWithAge.push(pet);
-}) 
-console.log(`pets With Age:`);
-console.log(petsWithAge);
+let petWithAge = pets.map (pet => {pet.age=getAge(pet);return pet;})
+console.log(petWithAge);
 
-let dogs = [];
-pets.forEach((val, index ) => {
-  let pet = pets[index];
-  if (pet.type === `dog`) {
-    dogs.push(pet);
-  }
-})
-console.log(`dogs:`);
+let dogs = pets.filter( pet =>  (pet.type ==="dog"));
 console.log(dogs);
 
-let jasper;
-pets.forEach((val,index) => {
-  let pet = pets[index];
-  if (pet.name === `Jasper`) {
-    jasper = pet;
-  }
-})
-console.log(`Jasper is ${jasper.age} years old`);
+let Jasper = pets.find( pet => ( pet.name === "Jasper"));
+console.log(`Jasper is ${Jasper.age} years old`);
